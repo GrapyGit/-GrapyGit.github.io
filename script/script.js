076095c4a471d2153dbf729bddccce958843b169
GrapyGit <76045081+GrapyGit@users.noreply.github.com>
2021-06-10 06:09:39 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function NewGame(){
+    localStorage.clear()
+    localStorage.setItem("Level", 0)
+    localStorage.setItem("Lives", 1)
+    localStorage.setItem("Armor", 1)
+    localStorage.setItem("Speed", 10)
+    localStorage.setItem("Power", 1)
+    localStorage.setItem("XP", 0)
+    localStorage.setItem("NewXP", 10)
+    localStorage.setItem("ochko", 0)
+    localStorage.setItem("LevP",1)
+}
+
