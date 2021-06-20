319a7c0485c051ddbae5011cb9a095dd05d021ea
GrapyGit <76045081+GrapyGit@users.noreply.github.com>
2021-06-20 16:16:21 +0300

    Update engine1.js

--- script/engine1.js
+++ script/engine1.js
@@ -12,6 +12,8 @@ var nomerdialoga = 0
 var fps;
 var mouseX,mouseY
 var time
+var duration
+var test = false
 var canvas = document.getElementById("myCanvas")
 var bounds = canvas.getBoundingClientRect()
 var ctx = canvas.getContext("2d")
@@ -20,7 +22,30 @@ canvas.height =canvas.offsetHeight
 document.getElementById('dead').style.left = document.getElementById('dead').offsetLeft - (document.getElementById('dead').offsetWidth/2) +"px"
 document.getElementById('dead').style.top = document.getElementById('dead').offsetTop - (document.getElementById('dead').offsetHeight/2) +"px"
 document.getElementById('dead').style.display = "none"
-for (nomerdialoga; dialogii[nomerdialoga][2] != Number(localStorage["Level"])+1; nomerdialoga++){}
+document.oncontextmenu = ()=>{return false}
+function propusk(a,b){
+	for (nomerdialoga; dialogii[nomerdialoga][2] != Number(localStorage["Level"])+a; nomerdialoga++){
+		if (dialogii[nomerdialoga][0] == "BLACK"){
+			window.location = "TITRI.html"; 
+		}
+	}
+    if (!b){
+		if (localStorage["book"]==null){
+			localStorage.setItem("book", 1)
+			alert("Вам открыта первая страница в учебнике")
+		}
+		else if (Number(localStorage["book"])==Number(localStorage["Level"])){
+			localStorage["book"] = Number(localStorage["Level"])+1
+			alert("У вас доступна новая информация в учебнике")
+		}
+
+	
+		Save()
+		location.reload();
+	}
+	document.getElementById("lol").style.display = "none"
+}
+
 function upgrade(a){
   hero.ochko--
   switch (a){
@@ -41,12 +66,14 @@ document.getElementById('lives').innerHTML=""+hero.maxLives
 document.getElementById('armor').innerHTML=""+hero.maxArmor
 document.getElementById('speed').innerHTML=""+hero.speed
 document.getElementById('power').innerHTML=""+hero.power
+levelUp()
   if (hero.ochko==0){
 	document.getElementById("plus").style.display = "none"
   }
 }
 
 var hero = {
+	img: preloadImages.list[2],
 	x : canvas.width-140,
 	y : 0,
 	speed : Number(localStorage.getItem("Speed")),
@@ -97,12 +124,8 @@ var hero = {
 		this.timer =  setInterval(()=>hero.Recovery(), 1000)
 	}
  }
+//hero.img.src = "img/igrock.png"
 
- document.getElementById('lives').innerHTML=""+hero.maxLives
- document.getElementById('armor').innerHTML=""+hero.maxArmor
- document.getElementById('speed').innerHTML=""+hero.speed
- document.getElementById('power').innerHTML=""+hero.power
- 
 
 if (hero.ochko==0) document.getElementById("plus").style.display = "none"
 
@@ -114,17 +137,19 @@ function dead(){
 	clearInterval(time)
 	enemies = []
     document.getElementById("opt").style.display = "none"
-	document.getElementById("zagolovok").style.display = "none"
+	document.getElementById("plus").style.display = "none"
+	//document.getElementById("zagolovok").style.display = "none"
+	document.getElementById("menu").style.display = "none"
 	document.getElementById("animation").style.display = "none"
     document.getElementById('dead').style.display = "block";
 }
 
 class help{
-	constructor(x,y,a,src,h,w){
+	constructor(x,y,a,img,h,w){
 		this.x = x
 		this.y = y
 		this.a =a
-		this.src = src
+		this.img =img
 		this.h = h
 		this.w =w
 		this.kill = false
@@ -132,13 +157,20 @@ class help{
 	}
 	tick(){
 		if (collision(this.x+ this.w/2,this.y+ this.h/2,this.w/2,hero.x+50,hero.y+70,50)){
-           switch(this.src){
-			   case "img/patrin.png":
+			console.log(this.img)
+			console.log(this.img.src.filename())
+			console.log(preloadImages.list[1].src)
+           switch(this.img.src.filename()){
+			   case "patrin":
+				console.log("где патроны")
 				hero.ammunition = hero.ammunition +getRandomInt(30);
 				document.getElementById("b").innerHTML =": " + hero.ammunition
 				   break;
-			   case "img/life.png":
-                 if (hero.lives < hero.maxLives)  hero.lives = hero.lives + getRandomInt(hero.maxLives - hero.lives)+1;
+			   case "life":
+                 if (hero.lives < hero.maxLives){  
+					 hero.lives = hero.lives + getRandomInt(hero.maxLives - hero.lives)+1
+					 console.log("жизни "+ hero.lives)
+				 };
 					break;
 		   }
 			  clearTimeout(this.timer)
@@ -150,10 +182,11 @@ class help{
 
 
 class enemie{
-	constructor(x,y,w,h,l,p,xp,a,src,ax,ay,speed){
+	constructor(x,y,w,h,l,p,xp,a,img,ax,ay,speed,f){
 		this.x = x
 		this.y= y
-		this.src = src
+		this.img = img
+	//	this.img.src = src
 		this.angle = a
 		this.w = w
 		this.h = h
@@ -166,7 +199,8 @@ class enemie{
 		this.x1 = 0
 		this.y1 =0
 		this.speed = speed
-		this.timer = setInterval(() => this.fire(), 2000);
+	    this.FireSpeed = f
+		this.timer = setInterval(() => this.fire(), this.FireSpeed);
 	}	
     tick(){
         if (this.x>this.ax) 
@@ -194,8 +228,8 @@ class enemie{
 				this.y = this.y + (this.ay-this.y);
 			 
 		if (this.x==this.ax && this.y==this.ay) {
-			this.ax = getRandomInt(canvas.width);
-			this.ay = getRandomInt(canvas.height);
+			this.ax = getRandomInt(canvas.width - 140);
+			this.ay = getRandomInt(canvas.height - 100);
 		}
 		this.angle =Math.atan2((hero.x+50) - (this.x+this.w/2),-(((hero.y+70)-(this.y + this.h/2))))*(180/Math.PI)
 	}
@@ -211,7 +245,7 @@ class enemie{
 	}
 	vzriv(i){
 		this.p++
-			draw(this.x,this.y,this.w,this.h,this.angle+180,this.src,this.x1,this.y1)
+			draw(this.x,this.y,this.w,this.h,this.angle+180,this.img,this.x1,this.y1)
 			if (this.p==10){
 				this.x1++
 				this.p = 0
@@ -257,24 +291,16 @@ class bullet {
 				musicArray.push(new music("music/explosion.mp3",1900,0.10,false))
 				musicArray[musicArray.length-1].play()
                 if (getRandomInt(101) > 50) {
-					switch(getRandomInt(2)){
-						case 0:
-							helps.push(new help(enemies[i].x,enemies[i].y,enemies[i].angle,"img/patrin.png",60,60))
-							break;
-						case 1:
-                            helps.push(new help(enemies[i].x,enemies[i].y,enemies[i].angle,"img/life.png",60,60))
-							break;
+							helps.push(new help(enemies[i].x,enemies[i].y,enemies[i].angle,preloadImages.list[getRandomInt(2)],60,60))
 					}
 				
-				}
-				//constructor(x,y,a,src,h,w)
+				
                clearInterval(enemies[i].timer)
 			   hero.xp += enemies[i].xp
-			   enemies[i].src = "img/vz.png"
+			   enemies[i].img = preloadImages.list[7]
 			   enemies[i].kill = false
 			   this.p = 0
 			   levelUp()
-			   
 			}
 			this.dx = 0.0;
 			this.dy = 0.0;
@@ -345,26 +371,30 @@ const K = {//перемещенние через клавиатуру
 
 function Leveles()
 {
-	console.log("уровень " +localStorage.getItem("Level"))
 	switch(Number(localStorage.getItem("Level"))){
 		case  0:
 			hero.ammunition = 30;
+			duration = 5000
 			initializationEnemie = [1,1,1,1,1];
 		break;
 		case  1:
 			hero.ammunition = 80;
+			duration = 4500
 			initializationEnemie = [1,2,2,1,2,2,2,1,2,1];
 		break;
 		case  2:
 			hero.ammunition = 100;
+			duration = 4000
 			initializationEnemie = [2,3,3,1,2,3,3,2,1,2,3,1,2,3,3,2];
 		break;
 		case  3:
 			hero.ammunition = 100;
+			duration = 3500
 			initializationEnemie = [2,4,3,4,3,4,4,3,2,2];
 		break;
 		case 4:
 			hero.ammunition = 200;
+			duration = 1000
 			initializationEnemie = [5];
 		break;
 	}
@@ -375,7 +405,6 @@ function Leveles()
 function deleat(i){
 	kill++
     enemies.splice(i,1)
-//	console.log(kill)
 }
 
 
@@ -394,20 +423,20 @@ function spawn(){
 	switch (getRandomInt(4)){
 		case 0://появление слева
             y = getRandomInt(canvas.height)
-			x =0
+			x =-100
 		break;
 
 		case 1://появленние сверху
 			x = getRandomInt(canvas.width) 
-			y = 0
+			y = -140
 		break;
 		case 2://появление справа
 			y = getRandomInt(canvas.height)
-			x = canvas.width 
+			x = canvas.width +100
 		break;
 		case 3://появление снизу
 			x = getRandomInt(canvas.width)
-			y = canvas.height 
+			y = canvas.height +140
 		break;
 	}
 	return {
@@ -429,9 +458,7 @@ function drawInfo() {
 	ctx.closePath();
   }
 
-function draw(x,y,w,h,a,sr,x1,y1){//анимация смерти
-	var img = new Image();
-	img.src = sr;
+function draw(x,y,w,h,a,img,x1,y1){//анимация смерти
 var dx = x + w/2
 	var dy = y + h/2
 	a=a*(Math.PI/180)
@@ -442,8 +469,7 @@ var dx = x + w/2
 	    ctx.translate(-dx,-dy)
 	}
     ctx.beginPath();
-    if (sr == "img/vz.png"){
-
+    if (x1 >=0){
         ctx.drawImage(img, 260*x1, 240*y1,260,240, x, y, w, h);
 	} 
 	else ctx.drawImage(img, x, y, w, h);
@@ -458,25 +484,25 @@ function vooor(){
 var kordinaty = spawn()
 switch(initializationEnemie[NumberOfEnemies]){
 	case 1:
-		enemies.push(new enemie(kordinaty.x,kordinaty.y,100,140,1,1,5,0,"img/vrag1.png",getRandomInt(canvas.width),getRandomInt(canvas.height),4))
+		enemies.push(new enemie(kordinaty.x,kordinaty.y,100,140,1,1,5,0,preloadImages.list[3],getRandomInt(canvas.width),getRandomInt(canvas.height),4,2000))
 	break;
 
 	case 2:
-		enemies.push(new enemie(kordinaty.x,kordinaty.y,100,140,2,1,10,0,"img/vragi2.png",getRandomInt(canvas.width),getRandomInt(canvas.height),6))
+		enemies.push(new enemie(kordinaty.x,kordinaty.y,100,140,2,1,10,0,preloadImages.list[4],getRandomInt(canvas.width),getRandomInt(canvas.height),6,2000))
 		break;
 
 		case 3:
-	enemies.push(new enemie(kordinaty.x,kordinaty.y,100,140,2,2,20,0,"img/vragi3.png",getRandomInt(canvas.width),getRandomInt(canvas.height),8))
+	enemies.push(new enemie(kordinaty.x,kordinaty.y,100,140,2,2,20,0,preloadImages.list[5],getRandomInt(canvas.width),getRandomInt(canvas.height),8,1000))
 		break;
 
 		case 4:
-	enemies.push(new enemie(kordinaty.x,kordinaty.y,100,140,3,2,40,0,"img/vragi4.png",getRandomInt(canvas.width),getRandomInt(canvas.height),10))
+	enemies.push(new enemie(kordinaty.x,kordinaty.y,100,140,3,2,40,0,preloadImages.list[6],getRandomInt(canvas.width),getRandomInt(canvas.height),10,1500))
 		break;
 		case 5:
-	enemies.push(new enemie(kordinaty.x,kordinaty.y,100,140,60,5,40,0,"img/igrock.png",getRandomInt(canvas.width),getRandomInt(canvas.height),10))
+	enemies.push(new enemie(kordinaty.x,kordinaty.y,100,140,60,5,40,0,preloadImages.list[2],getRandomInt(canvas.width),getRandomInt(canvas.height),10,500))
 		break;
 }
-enemies[enemies.length-1].fire()
+//enemies[enemies.length-1].fire(1)
 NumberOfEnemies++
 if (NumberOfEnemies==initializationEnemie.length){
 	clearInterval(time)
@@ -515,6 +541,8 @@ function NextLevel(){
 		document.getElementById("text").innerHTML=dialogii[nomerdialoga][1];
 			}
 			else {
+			document.body.style.cursor = "none"
+			document.getElementById("lol").style.display = "none"
 			document.getElementById("text").style.color = "white";
 			document.getElementById("history").style.bottom = "40%";
 			document.getElementById("history").style.backgroundColor = "black";
@@ -536,7 +564,8 @@ function NextLevel(){
 		hero.fire = true
 		NumberOfEnemies = 0
 		document.getElementById("opt").style.display = "block"
-		document.getElementById("zagolovok").style.display = "block"
+		document.getElementById("lol").style.display = "none"
+		document.getElementById("menu").style.display = "block"
 		document.getElementById("animation").style.display = "block"
 	    document.getElementById('history').style.display = "none";
 		document.body.style.backgroundImage = ''
@@ -550,7 +579,7 @@ function NextLevel(){
 		}
 		Save()
 		Leveles()
-		time = setInterval(vooor,5000);
+		time = setInterval(vooor,duration);
 		GameStart()
 
 		}
@@ -560,22 +589,23 @@ function GameStart(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 	if (kill != initializationEnemie.length && hero.lives > 0){
 	hero.angle = Math.atan2(mouseX - (hero.x+100/2),-(mouseY- (hero.y+140/2)))*(180/Math.PI)
-	draw(hero.x,hero.y,100,140,hero.angle,"img/igrock.png")
+	draw(hero.x,hero.y,100,140,hero.angle,hero.img)
 		for (var i=0; i<enemies.length; i++){
 			if (enemies[i].kill){
 				enemies[i].tick()
 				if (collision(enemies[i].x+(enemies[i].w/2),enemies[i].y+(enemies[i].h/2),enemies[i].w/2,hero.x+50,hero.y+70,50))
-				{	if (enemies[i].p != 5){
+				{
 					musicArray.push(new music("music/explosion.mp3",1900,0.10,false))
 					musicArray[musicArray.length-1].play()
 				   clearInterval(enemies[i].timer)
-				   enemies[i].src = "img/vz.png"
+				   enemies[i].img = preloadImages.list[7]
 				   enemies[i].kill = false
+					if (enemies[i].p != 5){
 				   hero.hit(hero.maxArmor)
-				} else  hero.hit(11)
-					
+					}else
+					hero.hit(10)
 				}
-				else draw(enemies[i].x,enemies[i].y,enemies[i].w,enemies[i].h,enemies[i].angle,enemies[i].src)
+				else draw(enemies[i].x,enemies[i].y,enemies[i].w,enemies[i].h,enemies[i].angle,enemies[i].img)
 			}
 			else enemies[i].vzriv(i)
 		}
@@ -585,10 +615,9 @@ function GameStart(){
 			  helps.splice(i,1)
 			}
 			else {
-			draw(helps[i].x,helps[i].y,helps[i].w,helps[i].h,helps[i].a,helps[i].src)
+			draw(helps[i].x,helps[i].y,helps[i].w,helps[i].h,helps[i].a,helps[i].img)
 			}
 		}
-
 		for (var i =0; i<bullets.length ;i++){
 			if (bullets[i].dx == 0 && bullets[i].dy ==0){
 				bullets.splice(i,1)
@@ -605,16 +634,33 @@ function GameStart(){
 	requestAnimationFrame(GameStart)
 			}
 			else if(hero.lives>0){
-				hero.fire = false
 				document.getElementById("opt").style.display = "none"
-				document.getElementById("zagolovok").style.display = "none"
+			    document.getElementById("menu").style.display = "none"
 				document.getElementById("animation").style.display = "none"
 				document.getElementById('history').style.display = "block";
+				if (test){
+					hero.fire = false
+					document.getElementById("text").style.color = "white";
+					document.getElementById("history").style.bottom = "40%";
+					document.getElementById("history").style.backgroundColor = "black";
+					document.getElementById("history").style.border = "none";
+					document.getElementById("nike").innerHTML=" ";
+					document.body.style.backgroundImage = "none";
+					document.body.style.backgroundColor = "black";
+					document.getElementById("text").innerHTML="Вы прошли тренировку, поздравляю :)";
+					document.getElementById("text").style.marginLeft = "0px";
+					document.getElementById("history").style.textAlign ="center";
+					setTimeout(()=>{ClearStorage(); window.location = "index.html"},4000)
+				}
+				else{
+				hero.fire = false
+				document.getElementById("lol").style.display = "block"
 				document.body.style.backgroundImage = 'url("img/1.gif")'
 				document.body.style.backgroundSize = "cover";
 				hero.lives = hero.maxLives
 				hero.armor = hero.maxArmor
                 NextLevel()
+				}
 			}
 	}
 
@@ -642,17 +688,6 @@ window.onresize = function(e) {
 	canvas.width = canvas.offsetWidth
 	canvas.height =canvas.offsetHeight 
 }
-document.getElementById("plus").onmouseover =()=>{hero.fire =false}
-document.getElementById("plus").onmouseout =()=>{hero.fire =true}
-
-document.addEventListener('keydown', K.fn);
-document.addEventListener('keyup', K.fn);//конец перемещенния
-
-Leveles();
-
-time = setInterval(vooor,5000);
-GameStart()
-
 function collision(x1,y1,r1,x2,y2,r2){
 	var x = x2 - x1
 	var y = y2 - y1
@@ -660,3 +695,49 @@ function collision(x1,y1,r1,x2,y2,r2){
 	var r = r1+r2
 	return l<=r
 }
+
+
+function FullScrean(){
+	if (document.fullscreenElement)
+	{
+		document.exitFullscreen()
+		document.getElementById("full").src = "img/full.png"
+	}
+	else {
+		document.documentElement.requestFullscreen();
+		document.getElementById("full").src ="img/NoFull.png"
+	}
+}
+
+
+var bol = true
+document.getElementById("plus").onmouseover =()=>{if(!hero.fire){ bol = false} else {bol = true} hero.fire =false}
+document.getElementById("plus").onmouseout =()=>{if (bol) hero.fire =true}
+document.getElementById("set").onmouseover =()=>{if(!hero.fire){ bol = false} else {bol = true} hero.fire =false}
+document.getElementById("set").onmouseout =()=>{if (bol) hero.fire =true}
+
+document.addEventListener('keydown', K.fn);
+document.addEventListener('keyup', K.fn);//конец перемещенния
+
+if (localStorage["initializationEnemie"]==null){
+Leveles()
+propusk(1,1)
+}
+else{
+    test =true
+	getStorage()
+	document.getElementById("lol").style.display = "none"
+	//initializationEnemie = [1,1,1,1,1];
+	console.log(initializationEnemie)
+  //  ClearStorage()
+}
+ document.getElementById('lives').innerHTML=""+hero.maxLives
+ document.getElementById('armor').innerHTML=""+hero.maxArmor
+ document.getElementById('speed').innerHTML=""+hero.speed
+ document.getElementById('power').innerHTML=""+hero.power
+time = setInterval(vooor,duration);
+GameStart()
+
+
+
+
